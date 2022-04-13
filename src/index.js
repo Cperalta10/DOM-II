6225323896e69eafd3fdede3c6ed66a9b94afaf9
Benardo Peralta <chrisperalta1998@gmail.com>
2022-04-12 22:36:38 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+// MOUSEOVER
+const logoHeader = document.querySelector(".logo-heading");
+// console.log(logoHeader);
+logoHeader.addEventListener(
+  "mouseover",
+  () => {
+    logoHeader.style.color = "blue";
+    setTimeout(() => {
+      logoHeader.style.color = "";
+    }, 4000);
+  },
+  false
+);
+
+// KEYDOWN
+document.addEventListener("keydown", (e) => {
+  //console.log(e.key);
+  if (e.key === "Escape") {
+    logoHeader.textContent = "Easter EGG!";
+  }
+});
+
+//
